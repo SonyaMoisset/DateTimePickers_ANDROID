601b2e5b0a134a7577071dbdf4584887ddc1fc81
smoisset <smoisset@worldremit.com>
2018-01-26 10:13:36 +0000

    Implement fragments in MainActivity

--- app/src/main/java/com/example/android/datetimepickers/MainActivity.java
+++ app/src/main/java/com/example/android/datetimepickers/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.datetimepickers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showDatePickerDialog(View view) {
+        android.support.v4.app.DialogFragment newFragment = new DatePickerFragment();
+
+        newFragment.show(getSupportFragmentManager(),
+                getString(R.string.date_picker));
+
+    }
+
+    public void showTimePickerDialog(View view) {
+        android.support.v4.app.DialogFragment newFragment = new TimePickerFragment();
+
+        newFragment.show(getSupportFragmentManager(),
+                getString(R.string.time_picker));
     }
 }
