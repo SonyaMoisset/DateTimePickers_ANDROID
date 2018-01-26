8e7a6d8286f5eccac64cfc53433004ef8bba4952
smoisset <smoisset@worldremit.com>
2018-01-26 10:05:34 +0000

    Fix import for DateFormat

--- app/src/main/java/com/example/android/datetimepickers/TimePickerFragment.java
+++ app/src/main/java/com/example/android/datetimepickers/TimePickerFragment.java
@@ -7,7 +7,8 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.widget.TimePicker;
 
-import java.text.DateFormat;
+import android.text.format.DateFormat;
+
 import java.util.Calendar;
 
 public class TimePickerFragment extends DialogFragment
